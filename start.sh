8a8cd24f5aa477ba5a3725441e95fdcd90fb06b4
Alexandria Burkleaux <burkleau@us.ibm.com>
2017-03-18 11:59:07 -0700

    Fix port in notebook DOn't gunzip data file Sleep before setting datasource

--- start.sh
+++ start.sh
@@ -7,8 +7,10 @@ sudo docker ps
 
 echo "Preparing data files"
 cd /home/localuser/ic2017-spark-monitoring-and-alerting
-gunzip faker-user-jobs.csv.gz
+#gunzip faker-user-jobs.csv.gz
 cp faker-user-jobs.csv /shared/data  
+
+sleep(5)
 
 echo "Configuring grafana"
 curl 'http://localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"ic2017","type":"graphite","url":"http://localhost:80","access":"proxy","isDefault":true}'
